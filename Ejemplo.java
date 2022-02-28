e05ea4f9587a825bb6f83cf48662042fa694c9d8
Julio Montes Torres <julmontor@uma.es>
2022-02-28 18:54:27 +0100

    Update Ejemplo.java
    Método privado

--- Ejemplo.java
+++ Ejemplo.java
@@ -2,6 +2,11 @@
 public class Ejemplo {
 	public static void main(String[] args) {
 		System.out.println("Nuevo texto");
+		cosa();
+	}
+
+	private static void cosa() {
+		System.out.println("Otro mensaje");
 	}
 	
 }
