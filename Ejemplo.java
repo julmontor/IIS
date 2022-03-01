3c641640a3af4e3b039edfcf0ca827b295389e36
Julio Montes Torres <julmontor@uma.es>
2022-03-01 18:11:56 +0100

    Método eliminado

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,12 +1,7 @@
 
 public class Ejemplo {
 	public static void main(String[] args) {
-		System.out.println("Nuevo texto");
-		cosa();
-	}
-
-	private static void cosa() {
-		System.out.println("Otro mensaje");
+		System.out.println("xxx");
 	}
 	
 }
