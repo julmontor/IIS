1b5608c13720ce26919704599d016fb2d4f1e7b7
Julio Montes Torres <julmontor@uma.es>
2022-03-02 21:18:28 +0100

    Saludos

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,10 +1,8 @@
 
 public class Ejemplo {
 	public static void main(String[] args) {
-		System.out.println("Salúdame más");
-		System.out.println("Qué tal");
 		System.out.println("Hola");
-		System.out.println("Buena tarde");
+		System.out.println("Adiós");
 	}
 	
 }
